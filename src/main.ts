17f98943daed1dd17cb2048c35c6adb01d465628
Chloe Brown Snyder <csny@seas.upenn.edu>
2018-03-18 16:47:14 -0400

    some procedural colors stuff

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,28 @@ let offsetsArray: Array<number>;
 let colorsArray: Array<number>;
 let n: number;
 
+function cosColor(t: number) : vec3
+{
+  // vector parameters for color cosine curve, red -> pink -> purple
+  var a = vec3.fromValues(.821, .328, .242);
+  var b = vec3.fromValues(.268, -2.92, .896);
+  var c = vec3.fromValues(.768, 1.078, .296);
+  var d = vec3.fromValues(2.820, 3.026, -0.273);
+
+  var scale = vec3.create();
+  var addCTD = vec3.create();
+  var scale2Pi = vec3.create();
+  vec3.scale(scale, c, t);
+  vec3.add(addCTD, scale, d);
+  vec3.scale(scale2Pi, addCTD, 6.28318);
+  var cosVec = vec3.fromValues(Math.cos(scale2Pi[0]), Math.cos(scale2Pi[1]), Math.cos(scale2Pi[2]));
+  var bCos = vec3.create();
+  vec3.mul(bCos, b, cosVec);
+  var toReturn = vec3.create();
+  vec3.add(toReturn, a, bCos);
+  return toReturn;
+}
+
 function setUpParticles()
 {
   particles = new Array<Particle>();
@@ -31,16 +53,19 @@ function setUpParticles()
   // Set up particles here. Hard-coded example data for now
   offsetsArray = [];
   colorsArray = [];
-  n = 100.0;
+  n = 10.0;
   var id = 0;
   
   for(let i = 0; i < n; i++) {
     for(let j = 0; j < n; j++) {
       var position = vec3.fromValues(i, j, 0);
       var velocity = vec3.fromValues(0, 0, 0);
-      var acceleration = vec3.fromValues(.001, 0, 0);
+      var acceleration = vec3.fromValues(0, 0, 0);
       var offset = vec3.fromValues(i, j, 0);
-      var color = vec4.fromValues(i/n, j/n, 1.0, 1.0);
+
+      var t = vec3.dist(position, vec3.fromValues(0,0,0)); // color takes distance from origin as input for time being. In future, update this to be distance from attractor/repellor force, i.e. mouseclick (x,y)
+      var vec3color = cosColor(t);
+      var color = vec4.fromValues(Math.max(0, vec3color[0]), Math.max(0, vec3color[1]), Math.max(0, vec3color[2]), 1.0);
       var currParticle = new Particle(position, velocity, acceleration, offset, color, id);
       particles.push(currParticle);
 
@@ -48,44 +73,17 @@ function setUpParticles()
       offsetsArray.push(j);
       offsetsArray.push(0);
 
-      colorsArray.push(i / n);
-      colorsArray.push(j / n);
-      colorsArray.push(1.0);
+      colorsArray.push(color[0]);
+      colorsArray.push(color[1]);
+      colorsArray.push(color[2]);
       colorsArray.push(1.0); // Alpha channel
       id++;
     }
   }
-  /*let offsets: Float32Array = new Float32Array(offsetsArray);
-  let colors: Float32Array = new Float32Array(colorsArray);
-  square.setInstanceVBOs(offsets, colors);
-  square.setNumInstances(n * n); // 10x10 grid of "particles"*/
 }
 
 function loadScene() {
   setUpParticles();
-  /*square = new Square();
-  square.create();
-
-  // Set up particles here. Hard-coded example data for now
-  let offsetsArray = [];
-  let colorsArray = [];
-  let n: number = 100.0;
-  for(let i = 0; i < n; i++) {
-    for(let j = 0; j < n; j++) {
-      offsetsArray.push(i);
-      offsetsArray.push(j);
-      offsetsArray.push(0);
-
-      colorsArray.push(i / n);
-      colorsArray.push(j / n);
-      colorsArray.push(1.0);
-      colorsArray.push(1.0); // Alpha channel
-    }
-  }
-  let offsets: Float32Array = new Float32Array(offsetsArray);
-  let colors: Float32Array = new Float32Array(colorsArray);
-  square.setInstanceVBOs(offsets, colors);
-  square.setNumInstances(n * n); // 10x10 grid of "particles"*/
 }
 
 function main() {
@@ -143,8 +141,12 @@ function main() {
     for(let i = 0; i < particles.length; i++)
     {
       let p = particles[i];
+      var fx = -.001;
+      var fy = -.001;
+    
+      p.applyForce(vec3.fromValues(fx, fy, 0));
       p.step(dt);
-
+     
       //update offsets array
       offsetsArray[i * 3] = p.currPos[0];
       offsetsArray[i * 3 + 1] = p.currPos[1];
